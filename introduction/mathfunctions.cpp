b57dd7f58beaae737affb5b185c1c558ce334489
5fatya <149310230+5fatya@users.noreply.github.com>
2024-11-09 17:27:31 +0100

    intro to cpp

---
+++ introduction/mathfunctions.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+int main() {
+    int x = -5;
+    int absoluteValue = abs(x);
+    std::cout << "Absolute value of " << x << " is: " << absoluteValue << std::endl;
+    return 0;
+}
+
