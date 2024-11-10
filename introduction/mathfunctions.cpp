264c49bbc2fcf4369be30ccbd1a0d51fe2dc2ea1
Fatima Fares <149310230+5fatya@users.noreply.github.com>
2024-11-10 14:18:03 +0100

    mathfunctions.cpp

--- introduction/mathfunctions.cpp
+++ introduction/mathfunctions.cpp
@@ -2,8 +2,27 @@
 #include <cmath>
 int main() {
     int x = -5;
-    int absoluteValue = abs(x);
-    std::cout << "Absolute value of " << x << " is: " << absoluteValue << std::endl;
+    int absoluteValue = abs(x); // function of absolute value is abs()
+    cout << "Absolute value of " << x << " is: " << absoluteValue << std::endl;
+   
+   double p = 25.0;
+   double squareRoot = sqrt(p); // the function for square root is sqrt()
+cout << "square root of " << p << "is" << squareRoot << endl;
+
+double m = 27.0;
+double cubeRoot = cbrt(m); // function for cube root is cbrt()
+cout << "cube root of " << m << " is " << cubeRoot << endl;
+
+int f = 2;
+int j = 3;
+
+int result = pow(f, j); // pow(f, j) this function multiplies x to the power of y 
+cout << "result of " << f << " to the power of " << j << " is:" << result << endl;
+
+double e = 1.0; 
+double result2 = exp(e); // function that multiples the value 2.718 to the power of x 
+cout << "result of e to the power of " << e << " is " << result2 << endl;
+
     return 0;
 }
 
